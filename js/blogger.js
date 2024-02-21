327f4a6b2db65391c03aee73d2bbadf55c96e31a
Larry Moiola <lmoiola@gmail.com>
2024-02-20 16:49:49 -0800

    fixing typo in month name

--- js/blogger.js
+++ js/blogger.js
@@ -113,7 +113,7 @@ function getLatestPost() {
     var month = date_pub.getUTCMonth();
     var months = {
       0: "January",
-      1: "Februrary",
+      1: "February",
       2: "March",
       3: "April",
       4: "May",
