6900774aecf85f1f42d345e5456c2162de6d4cd1
Larry Moiola <lmoiola@gmail.com>
2025-11-23 12:30:36 -0800

    changing header and footer color to rgb(42,43,51)

--- js/header-anim.js
+++ js/header-anim.js
@@ -2,7 +2,7 @@
 // https://codepen.io/nickcil/pen/sfutl
 // https://stackoverflow.com/questions/13725263/changing-background-opacity-of-div-using-rgba
 $(window).scroll(function(){
-    $("header").css("background-color", 'rgba(51, 51, 51,' + (0.0 + $(window).scrollTop() / 300) + ')');
+    $("header").css("background-color", 'rgba(43, 42, 51,' + (0.0 + $(window).scrollTop() / 300) + ')');
     $(".hero_text").css("opacity", 1.0 - $(window).scrollTop()/350);
     $("#hero_title_card").css("opacity", 1.25 - $(window).scrollTop()/190);
     // console.log(1.0 - $(window).scrollTop()/600);
